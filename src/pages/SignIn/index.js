5de2c1d3387110c8c8f14bb4aa2e2d966700457e
Diego Azevedo <dapptn@hotmail.com>
2020-02-03 17:30:47 +1100

    Form sign in done

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
 
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
 const SignIn = () => {
-  return <Background></Background>;
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Type your email address"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Type your password"
+          />
+
+          <SubmitButton onPress={() => {}}>Access</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Create your account</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
 };
 
 export default SignIn;
