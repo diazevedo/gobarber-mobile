083403baf138b152a4661d11aea1d2e9db225bbc
Diego Azevedo <dapptn@hotmail.com>
2020-02-04 22:47:31 +1100

    Routes when signed and dashboard page added

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
+
 import createStore from './createStore';
+import persistReducer from './persistReducer';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
-
-import persistReducer from './persistReducer';
 
 const sagaMonitor = __DEV__ ? console.tron.createSagaMonitor() : null;
 
