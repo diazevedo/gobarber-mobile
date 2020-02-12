5af97928ea64ea1f6b489418f4afc7711b4a178b
Diego Azevedo <dapptn@hotmail.com>
2020-02-12 20:39:12 +1100

    Code-push ios

--- ios/gobarberMobile/AppDelegate.m
+++ ios/gobarberMobile/AppDelegate.m
@@ -7,6 +7,8 @@
 
 #import "AppDelegate.h"
 
+#import <CodePush/CodePush.h>
+
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -35,7 +37,7 @@
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 
